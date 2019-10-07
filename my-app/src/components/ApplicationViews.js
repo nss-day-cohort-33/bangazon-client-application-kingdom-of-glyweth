7ff374ab75e6920a961db292d30557773318dd4a
joekennerly <joseph.kennerly@gmail.com>
2019-10-07 13:14:55 -0500

    init

--- my-app/src/components/ApplicationViews.js
+++ my-app/src/components/ApplicationViews.js
@@ -1,11 +1,11 @@
-// import { Route } from "react-router-dom"
+import { Route } from "react-router-dom"
 import React from "react"
-// import { withRouter } from "react-router-dom"
+import { withRouter } from "react-router-dom"
 
-// const ApplicationViews = () => {
-//     return (
-//       <h1>YoYo!</h1>
-//     )
-// }
+const ApplicationViews = () => {
+    return (
+      <h1>YoYo!</h1>
+    )
+}
 
-// export default withRouter(ApplicationViews)
+export default withRouter(ApplicationViews)
