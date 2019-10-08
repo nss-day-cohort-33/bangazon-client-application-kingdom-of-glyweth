701a14696e1b0080601752b0bd4a8c54150db804
MelanieJaneBond <miss.bond2@gmail.com>
2019-10-08 09:31:15 -0500

    not sure why there is code on master

--- my-app/src/components/ApplicationViews.js
+++ my-app/src/components/ApplicationViews.js
@@ -8,4 +8,33 @@ const ApplicationViews = () => {
     )
 }
 
+const getSingleProduct = (productId) => {
+  if (isAuthenticated()) {
+      fetch(`http://localhost:8000/products?product=${productId}`, {
+          "method": "GET",
+          "headers": {
+              "Accept": "application/json",
+              "Authorization": `Token ${localStorage.getItem("kennywood_token")}`
+          }
+      })
+          .then(response => response.json())
+          // .then((allAttractions) => {
+          //     setAttractions(allAttractions)
+          // })
+  }
+}
+
+  <Route exact path="/products/:each(\d+)" render={(props) => {
+              // Find the product with the id of the route parameter
+              let product = this.state.products.find(product =>
+                  product.id === parseInt(props.match.params.each)
+              )
+              // If the product wasn't found, create a default one
+              if (!product) {
+                  product = {id:404, name:"404", breed: "Dog not found"}
+              }
+
+              return <ProductDetail product={getSingleProduct} />
+            }} />
+
 export default withRouter(ApplicationViews)
