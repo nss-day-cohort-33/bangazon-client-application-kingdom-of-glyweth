03175b3fc5b74296b05e1b0930e63cd4dabe8580
joekennerly <joseph.kennerly@gmail.com>
2019-10-14 12:56:35 -0500

    made change

--- my-app/src/modules/APImanager.js
+++ my-app/src/modules/APImanager.js
@@ -2,56 +2,51 @@ const remoteURL = "http://localhost:8000"
 
 export default {
     get(resource, id) {
-        // if isAuthenticated() {
-      return fetch(`${remoteURL}/${resource}/${id}`, {
-          "method": "GET",
-          "headers": {
-              "Accept": "application/json",
-              "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
-          }
-      }).then(response => response.json())
-    //   }
+        return fetch(`${remoteURL}/${resource}/${id}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: `Token ${localStorage.getItem("bangazon_token")}`
+            }
+        }).then(response => response.json())
     },
     getAll(resource) {
-        // if (isAuthenticated()) {
-            return fetch(`${remoteURL}/${resource}`, {
-                "method": "GET",
-                "headers": {
-                    "Accept": "application/json",
-                    "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
-                }
-            })
-                .then(response => response.json())
-        // }
+        return fetch(`${remoteURL}/${resource}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: `Token ${localStorage.getItem("bangazon_token")}`
+            }
+        }).then(response => response.json())
     },
     post(resource, newObject) {
-      return fetch(`${remoteURL}/${resource}`, {
-        "method": "POST",
-        "headers": {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-          "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
-        },
-        body: JSON.stringify(newObject)
-      }).then(data => data.json())
+        return fetch(`${remoteURL}/${resource}`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Token ${localStorage.getItem("bangazon_token")}`
+            },
+            body: JSON.stringify(newObject)
+        })
     },
     put(resource, resourceObject) {
-      return fetch(`${remoteURL}/${resource}/${resourceObject.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        "body": JSON.stringify(resourceObject)
-      }).then(data => data.json());
+        return fetch(`${remoteURL}/${resource}/${resourceObject.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(resourceObject)
+        }).then(data => data.json())
     },
     delete(resource) {
         // if (isAuthenticated()) {
-            return fetch(`${remoteURL}/${resource}/${resource.id}`, {
-                "method": "DELETE",
-                "headers": {
-                "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
-                }
-            })
+        return fetch(`${remoteURL}/${resource}/${resource.id}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Token ${localStorage.getItem("bangazon_token")}`
+            }
+        })
         // }
     }
-  }
+}
