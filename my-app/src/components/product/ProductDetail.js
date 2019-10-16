053c27e79e25a7c08bb081d1d55d703f79bdbca9
Misty DeRamus <mistyderamus@gmail.com>
2019-10-16 10:53:56 -0500

    cleaned up comments

--- my-app/src/components/product/ProductDetail.js
+++ my-app/src/components/product/ProductDetail.js
@@ -3,26 +3,7 @@ import APImanager from "../../modules/APImanager"
 
 const Product = props => {
     const[confirmation, setConfirmation] = useState("")
-  // make new order for customer
-//   const makeNewOrder = props => {
-//     return fetch("http://localhost:8000/order", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Accept: "application/json",
-//         Authorization: `Token ${localStorage.getItem("bangazon_token")}`
-//       },
-//       body: JSON.stringify({
-//         customer_id: `${localStorage.getItem(`customer_id`)}`
-//       })
-//     }).then(res => res.json())
-//   }
 
-  //I don't know where the specific fetch is going to go
-  //We need to call in one product and gather all of its details somehow
-  // users will be clicking links throughout the app that can direct them to this detail page
-  //What's the route of each link? "/products/1" or "/products/3", etc.
-  // I'll mock up the call and get back to it when the boys have sorted the user questions.
 
   const addProduct = id => {
     APImanager.post("order",{"product_id":id})
