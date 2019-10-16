d377a10fadad9eaf45210c762bfae06d18963d0b
berkeley platte <berkeleyplatte989@gmail.com>
2019-10-16 10:17:29 -0500

    gonna try something

--- my-app/src/components/productCategory/ProductByCategory.js
+++ my-app/src/components/productCategory/ProductByCategory.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
-import Product from "../product/Product";
 
 const ProductByCategory = props => {
   return (
