537dcdeb1a3bcc672d6014d57196cd511d5f7c64
joekennerly <joseph.kennerly@gmail.com>
2019-10-17 11:45:06 -0500

    added handle payment function

--- my-app/src/components/cart/Cart.js
+++ my-app/src/components/cart/Cart.js
@@ -1,8 +1,10 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useRef} from "react"
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
 
 const Cart = () => {
     const [orders, setOrders] = useState([])
+    const [payments, setPayments] = useState([])
+    const paymentType = useRef()
     const { isAuthenticated } = useSimpleAuth()
     const getOrders = () => {
         if (isAuthenticated()) {
@@ -16,15 +18,47 @@ const Cart = () => {
                 .then(setOrders)
         }
     }
+    const getPayments = () => {
+        if (isAuthenticated()) {
+            fetch(`http://localhost:8000/payment?customer=current`, {
+                "method": "GET",
+                "headers": {
+                    "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
+                }
+            })
+                .then(response => response.json())
+                .then(setPayments)
+        }
+    }
+    const handlePayment = (id) => {
+        const newPayment = {
+            "order_id": id,
+            "payment_id": +paymentType.current.firstChild.id
+        }
+        console.log("payment id", newPayment )
+    }
 
-    useEffect(getOrders,[])
+    useEffect(() => {
+        getOrders()
+        getPayments()
+    },[])
 
+    console.log(orders)
     return (
         <>
         <h1>HEY THIS IS CART</h1>
         {orders.map(order=> {
-            return <li key={order.id}>{order.id}</li>
+            return(
+            <div>
+            <li key={order.id}>{order.id}</li>
+            <button onClick={()=>handlePayment(order.id)}>Ready to Checkout</button>
+            </div>)
         })}
+        <select ref={paymentType}>
+            {payments.map(payment => {
+                return <option id={payment.id} key={payment.id}>{payment.merchant_name.toUpperCase()} .... {payment.account_number.slice(-4)}</option>
+            })}
+        </select>
         </>
     )
 }
