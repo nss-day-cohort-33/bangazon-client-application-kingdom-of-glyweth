0a1a6acc969b47894ef14ba1ed32e9d7dc5c1088
Alex Rumsey <alexrumsey1993@gmail.com>
2019-10-17 11:41:15 -0500

    cart now lists items in customers cart by customer, customer can delete items from cart.

--- my-app/src/components/cart/Cart.js
+++ my-app/src/components/cart/Cart.js
@@ -1,30 +1,91 @@
 import React, {useState, useEffect} from "react"
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
+import { Item } from "semantic-ui-react"
 
 const Cart = () => {
     const [orders, setOrders] = useState([])
     const { isAuthenticated } = useSimpleAuth()
     const getOrders = () => {
         if (isAuthenticated()) {
-            fetch(`http://localhost:8000/order`, {
+            fetch(`http://localhost:8000/order?payment_id=None`, {
                 "method": "GET",
                 "headers": {
                     "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
                 }
             })
                 .then(response => response.json())
-                .then(setOrders)
+                .then(order => {
+                    setOrders(order)
+                })
         }
-    }
+    };
 
     useEffect(getOrders,[])
 
+    // const removeProductFromOrder = id => {
+    //     fetch(`http://localhost:8000/order_product/${id}`, {
+    //       method: "DELETE",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         Accept: "application/json",
+    //         Authorization: `Token ${localStorage.getItem("bangazon_token")}`
+    //         }
+    //     }).then(() => getOrders);
+    // };
+
+    const deleteProductFromCart = (item_id) => {
+        fetch(`http://localhost:8000/order/${orders[0].id}`, {
+          method: "PUT",
+          headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
+          },
+          body: JSON.stringify({
+            payment_id: null,
+            item_id: item_id
+          })
+        })
+        .then(getOrders)
+      }
+
     return (
         <>
-        <h1>HEY THIS IS CART</h1>
-        {orders.map(order=> {
-            return <li key={order.id}>{order.id}</li>
-        })}
+            <h1>My Cart:</h1>
+            {orders.map(item=> {
+                return item.line_items.map(item => {
+                    return (
+                        <div>
+                            <div class="card" style={{ margin: "2em", width: "22rem" }}>
+                                <div class="card-body">
+                                    <h2 class="card-name">
+                                        <strong>{item.name}</strong>
+                                    </h2>
+                                    <div class="card-text">
+                                        <strong>Price:</strong>{item.price}
+                                    </div>
+                                    <br></br>
+                                    <p class="card-text">{item.description}</p>
+                                    <div
+                                    style={{
+                                    margin: "1em .5em 0 .5em",
+                                    display: "flex",
+                                    justifyContent: "space-between"
+                                    }}
+                                    >
+                                    <button
+                                    class="btn btn-danger"
+                                    onClick={() => deleteProductFromCart(item.id)}
+                                    >
+                                    Remove
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    );
+                });
+            })};
         </>
     )
 }
